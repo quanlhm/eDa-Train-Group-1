8909dec92dde208544ba2df19ebc5049fda76b50
hung230206 <hungtranbeta@gmail.com>
2026-01-23 17:14:38 +0700

    asset 1

---
+++ Train-01-001/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    // setting slide
+    currentSlide = "001";
+    localStorage.setItem('current-slide', currentSlide);
+    disableSwipe = false;
+
+    //animation, if have serveral class with the same time appear, delcare in an array
+    const animations = {
+        300: [".title"],
+        500: [".content"],
+        700: [".product"]
+    };
+
+    for (const [offset, elem] of Object.entries(animations)) {
+        setTimeout(() => elem.forEach((e) => $(e).addClass("show")), offset);
+    }
+
+    // flow-slide
+    var el = document.getElementById('container');
+    swipedetect(el, function (swipedir) {
+        if (swipedir == 'left') {
+          
+        }
+        if (swipedir == 'right') {
+             window.goToSlide("002");
+           
+        }
+    });
+});
